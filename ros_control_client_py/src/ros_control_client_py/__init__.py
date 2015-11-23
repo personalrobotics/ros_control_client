212094f2347cefd8a33a7d26369cd93d27904e4f
Clint Liddick <clint.liddick@gmail.com>
2015-11-23 12:43:40 -0500

    move adapy's ros_control python code into its own library

---
+++ ros_control_client_py/src/ros_control_client_py/__init__.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+
+from controller_client import ControllerManagerClient, ControllerSwitcher, JointStateClient, ROSControlError, SwitchError
+from trajectory_client import FollowJointTrajectoryClient, TrajectoryFuture, TrajectoryExecutionFailed
